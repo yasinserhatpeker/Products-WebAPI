7151faf64b239fff75ac43fd46b6cceec7c2b745
Yasin <yasinpeker43@gmail.com>
2025-09-05 19:54:53 +0300

    feat(controller) add Login

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,11 +9,13 @@ namespace ProductsAPI.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private UserManager<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UsersController(UserManager<AppUser> userManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         [HttpPost("register")]
 
@@ -34,7 +36,7 @@ namespace ProductsAPI.Controllers
             {
                 return NotFound();
             }
-            var result = await _userManager.CreateAsync(user,model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
                 return StatusCode(201);
@@ -42,5 +44,20 @@ namespace ProductsAPI.Controllers
             return BadRequest(result.Errors);
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Email is invalid" });
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (result.Succeeded)
+            {
+                return Ok(new { token = "token" });
+            }
+            return Unauthorized();
+        }
     }
 }
